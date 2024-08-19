653d15e8d64eb20ec50f4d424f4339e2a3233cbe
djshampz904 <martin.kimani@strathmore.edu>
2024-08-19 14:50:13 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -7,6 +7,14 @@ import math
 from typing import List, Tuple, Dict
 
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -43,17 +51,15 @@ class Server:
         return (page - 1) * page_size, page * page_size
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
-        total_items = len(self.dataset())
-        total_pages = (total_items + page_size - 1) // page_size
-        data = self.get_page(page, page_size)
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
-        return {
-            "page_size": page_size,
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
+        return page_info
